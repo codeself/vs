dc70d9ac7da88ae657bcfc0b7b67f871d16113c1
Jing Chen <jchenvi@hotmail.com>
2023-03-13 07:24:36 +0000

    endpoint: add load so code
    Type: feature

---
+++ src/endpoint/comm.h
@@ -0,0 +1,18 @@
+#ifndef _COMM_H_
+#define _COMM_H_
+
+typedef uint8_t  unsigned char
+typedef uint16_t unsigned short
+typedef uint32_t unsigned int
+typedef uint64_t unsigned long long
+
+#define VS_OK	0
+#define VS_ERR	1
+
+#define VS_IP_MAX_LEN	40
+#define VS_PORT_MAX_LEN	6
+#define VS_PATH_MAX_LEN	256
+#define VS_FILE_MAX_LEN	128
+#define VS_CPT_MAX_LEN	64
+
+#endif
---
+++ src/endpoint/load_module.h
@@ -0,0 +1,8 @@
+#ifndef _LOAD_MODULE_H_
+#define _LOAD_MODULE_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif
